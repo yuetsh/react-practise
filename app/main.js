b3dc57b901aa3b4907fa1a8b6363a55b59e9e91d
yuetsh <517252939@qq.com>
2015-10-03 10:36:12 +0800

    git init.

---
+++ app/main.js
@@ -0,0 +1,11 @@
+"use strict";
+
+import 'semantic-ui/semantic.min.css!';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CommentBox from './comment/CommentBox';
+
+ReactDOM.render(
+  <CommentBox></CommentBox>,
+  document.getElementById('header')
+);
